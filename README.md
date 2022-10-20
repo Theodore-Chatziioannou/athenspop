9ca3be3a5354feb128dbba19ed5fc438a2dae0c0
Theodore.Chatziioannou <Theodore.Chatziioannou@arup.com>
2022-10-20 21:39:05 +0100

    setup directories

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # athenspop
+
+
+
+# Introduction
+The aim of this repo is to demonstrate the creation of a synthetic MATSim population for Athens, using the [PAM](https://github.com/arup-group/pam) library.
+
+# Installation
+
+You can install the library as follows:
+
+```
+virtualenv -p python3
+pip3 install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Packaging settings."""
+import os
+from setuptools import find_packages, setup
+
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="athenspop",
+    version='0.1',
+    python_requires='>=3.8',
+    description="Building a synthetic population for Athens",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["athenspop = athenspop.cli:cli"]},
+    install_requires=install_requires,
+)
